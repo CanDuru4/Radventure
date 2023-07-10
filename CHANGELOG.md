845166893b23c36387e4d624bd380571f7bd4dca
Can Duru <canduru2004@gmail.com>
2023-07-10 23:41:14 +0300

    Created .gitignore, README, CHANGELOG. Removed main.storyboard.

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+Radventure Application for multipurpose usage;
+
+## [0.0.0] (1) (2023-07-10)
+- Ver. 0.0.0 build is initalized for development. 
---
+++ Radventure/TabBarViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TabBarViewController.swift
+//  Radventure
+//
+//  Created by Can Duru on 22.06.2023.
+//
+
+//MARK: Import
+import UIKit
+
+class TabBarViewController : UITabBarController {
+    
+//MARK: Load
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().barTintColor = .systemBackground
+        tabBar.backgroundColor = .systemBackground
+        tabBar.tintColor = .label
+        setupVCs()
+    }
+    
+    //MARK: Create ViewControllers
+    func setupVCs() {
+          viewControllers = [
+            createNavController(for: HomeMapViewController(), title: NSLocalizedString("", comment: ""), image: UIImage(systemName: "map.fill")!.withRenderingMode(.alwaysOriginal).withTintColor(.systemBlue)),
+              createNavController(for: ProfileViewController(), title: NSLocalizedString("", comment: ""), image: UIImage(systemName: "flag.checkered.2.crossed")!.withRenderingMode(.alwaysOriginal).withTintColor(.systemBlue)),
+              createNavController(for: ScoreboardViewController(), title: NSLocalizedString("", comment: ""), image: UIImage(systemName: "person.fill")!.withRenderingMode(.alwaysOriginal).withTintColor(.systemBlue)),
+          ]
+      }
+    
+    //MARK: Set Tabbar Items
+    var i = -1
+    fileprivate func createNavController(for rootViewController: UIViewController,
+                                                    title: String,
+                                                    image: UIImage) -> UIViewController {
+          let items = ["Map", "Scoreboard", "Profile"]
+          i = i+1
+          let navController = UINavigationController(rootViewController: rootViewController)
+
+          navController.tabBarItem.title = items[i]
+          navController.tabBarItem.image = image
+          navController.navigationBar.prefersLargeTitles = false
+          rootViewController.navigationItem.title = title
+          return navController
+      }
+}
