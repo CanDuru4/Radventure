28c81c53ef70668ae7422d813ab5bd197cb2ad18
Can Duru <canduru2004@gmail.com>
2023-07-18 00:59:49 +0300

    Updated assets. Updated info.plist. Created login page.

--- Radventure/Home/HomeMapViewController.swift
+++ Radventure/Home/HomeMapViewController.swift
@@ -6,14 +6,156 @@
 //
 
 import UIKit
+import MapKit
 
-class HomeMapViewController: UIViewController {
+class HomeMapViewController: UIViewController, MKMapViewDelegate {
 
+//MARK: Set Up
+    
+    var coordinate = CLLocationCoordinate2D(latitude: 41.066993155414536, longitude: 29.034859552149705)
+
+
+    //MARK: Map Setup
+    private let map: MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
+    
+
+    
+//MARK: Load
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(named: "AppColor1")
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        //MARK: Map Load
+        view.addSubview(map)
+        setMapLayout()
+        mapLocation()
+        setButton()
+        map.delegate = self
+    }
+   
+    
+//MARK: Map
+    func mapLocation(){
+        LocationManager.shared.getUserLocation { [weak self] location in DispatchQueue.main.async {
+                guard let strongSelf = self else {
+                    return
+                }
+            strongSelf.map.setRegion(MKCoordinateRegion(center: self!.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)), animated: true)
+                strongSelf.map.showsUserLocation = true
+            }
+        }
+    }
+    
+//    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+//
+//            var span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+//            var region = MKCoordinateRegion(center: self.coordinate, span: span)
+//            mapView.setRegion(region, animated: true)
+//
+//    }
+    
+    func setMapLayout(){
+        map.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([map.topAnchor.constraint(equalTo: view.topAnchor), map.bottomAnchor.constraint(equalTo: view.bottomAnchor), map.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor), map.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)])
+    }
+    
+    
+    
+//MARK: Buttons Setup
+    func setButton(){
+        
+        
+        //MARK: Current Location Button
+        let currentlocationButton = UIButton(type: .custom)
+        currentlocationButton.backgroundColor = UIColor(white: 1, alpha: 0.8)
+        currentlocationButton.setImage(UIImage(systemName: "location.fill")?.resized(to: CGSize(width: 25, height: 25)).withTintColor(.systemBlue), for: .normal)
+        currentlocationButton.addTarget(self, action: #selector(pressed), for: .touchUpInside)
+        view.addSubview(currentlocationButton)
+        
+        currentlocationButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([currentlocationButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10), currentlocationButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30), currentlocationButton.widthAnchor.constraint(equalToConstant: 50), currentlocationButton.heightAnchor.constraint(equalToConstant: 50)])
+        currentlocationButton.layer.cornerRadius = 25
+        currentlocationButton.layer.masksToBounds = true
+        
+        //MARK: Zoom Out Button
+        let zoomOutButton = UIButton(type: .custom)
+        zoomOutButton.backgroundColor = UIColor(white: 1, alpha: 0.8)
+        zoomOutButton.setImage(UIImage(systemName: "minus.square.fill")?.resized(to: CGSize(width: 25, height: 25)).withTintColor(.systemBlue), for: .normal)
+        zoomOutButton.addTarget(self, action: #selector(zoomOut), for: .touchUpInside)
+        view.addSubview(zoomOutButton)
+        
+        zoomOutButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([zoomOutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10), zoomOutButton.bottomAnchor.constraint(equalTo: currentlocationButton.topAnchor, constant: -20), zoomOutButton.widthAnchor.constraint(equalToConstant: 50), zoomOutButton.heightAnchor.constraint(equalToConstant: 50)])
+        zoomOutButton.layer.cornerRadius = 10
+        zoomOutButton.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner] // Top right corner, Top left corner respectively
+        
+        //MARK: Zoom In
+        let zoomInButton = UIButton(type: .custom)
+        zoomInButton.backgroundColor = UIColor(white: 1, alpha: 0.8)
+        zoomInButton.setImage(UIImage(systemName: "plus.square.fill")?.resized(to: CGSize(width: 25, height: 25)).withTintColor(.systemBlue), for: .normal)
+        zoomInButton.addTarget(self, action: #selector(zoomIn), for: .touchUpInside)
+        view.addSubview(zoomInButton)
+        
+        zoomInButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([zoomInButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10), zoomInButton.bottomAnchor.constraint(equalTo: zoomOutButton.topAnchor), zoomInButton.widthAnchor.constraint(equalToConstant: 50), zoomInButton.heightAnchor.constraint(equalToConstant: 50)])
+        zoomInButton.layer.cornerRadius = 10
+        zoomInButton.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner] // Top right corner, Top left corner respectively
     }
 
-
+    //MARK: Current Location Button Action
+    @objc func pressed() {
+        LocationManager.shared.getUserLocation { [weak self] location in DispatchQueue.main.async {
+                guard let strongSelf = self else {
+                    return
+                }
+                strongSelf.map.setRegion(MKCoordinateRegion(center: location.coordinate, span:MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)), animated: true)
+            }
+        }
+    }
+    
+var zoom_count = 14
+//MARK: Zoom In Button Action
+@objc func zoomIn() {
+    zoomMap(byFactor: 0.5)
+    zoom_count = zoom_count-1
 }
 
+//MARK: Zoom Out Button Action
+@objc func zoomOut() {
+    if zoom_count < 14 {
+        zoomMap(byFactor: 2)
+        zoom_count = zoom_count+1
+    }
+}
+    
+    
+    
+//MARK: Zoom in and Out Function
+    func zoomMap(byFactor delta: Double) {
+        var region: MKCoordinateRegion = self.map.region
+        var span: MKCoordinateSpan = map.region.span
+        span.latitudeDelta *= delta
+        span.longitudeDelta *= delta
+        region.span = span
+        map.setRegion(region, animated: true)
+    }
+}
+
+//MARK: Image Resize Extension
+extension UIImage {
+    public func resized(to target: CGSize) -> UIImage {
+        let ratio = min(
+            target.height / size.height, target.width / size.width
+        )
+        let new = CGSize(
+            width: size.width * ratio, height: size.height * ratio
+        )
+        let renderer = UIGraphicsImageRenderer(size: new)
+        return renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: new))
+        }
+    }
+}
