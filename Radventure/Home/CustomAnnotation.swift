d1b55b68118672a70bafde77f5b6ed7288ac46ae
Can Duru <canduru2004@gmail.com>
2023-07-18 22:19:03 +0300

    Updated TabBarController and removed profile page. Updated HomeViewController and added features: starting & stopping the activity, answering questions, timer, rules, log out, custom pin annotation. Updated LogInViewController features and activated firebase log in.

---
+++ Radventure/Home/CustomAnnotation.swift
@@ -0,0 +1,14 @@
+//
+//  CustomAnnotation.swift
+//  Radventure
+//
+//  Created by Can Duru on 18.07.2023.
+//
+
+import UIKit
+import MapKit
+
+class CustomPointAnnotation: MKPointAnnotation {
+    var customidentifier: String!
+    var pinCustomImageName:String!
+}
